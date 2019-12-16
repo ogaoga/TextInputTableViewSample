cba386598409483f4a1c96abae6a52bcd1a9e852
ogaoga <tsutomu.ogasawara+github@gmail.com>
2019-12-16 23:51:38 +0900

    Add initial files

---
+++ TextInputTableViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextInputTableViewSample
+//
+//  Created by ogaoga on 2019/12/16.
+//  Copyright © 2019 ogaoga.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
